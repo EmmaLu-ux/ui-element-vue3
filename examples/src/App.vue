1014b3ec9acd7eff2b75c97186337ff7f84200a8
EmmaLu-ux <genhualu22@gmail.com>
2025-07-18 16:21:11 +0800

    feat(button): generate button private variable

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 // import { UeButtonGroup, UeButton } from "../../packages"
-import ButtonExamples from "./components/ButtonExamples.vue"
+// import ButtonExamples from "./components/ButtonExamples.vue"
+import ButtonGroupExp from "./components/ButtonGroupExp.vue"
 </script>
 
 <template>
-  <ButtonExamples></ButtonExamples>
+  <!-- <ButtonExamples></ButtonExamples> -->
+  <ButtonGroupExp></ButtonGroupExp>
 </template>
 
 <style scoped></style>
---
+++ examples/src/components/ButtonGroupExp.vue
@@ -0,0 +1,13 @@
+<template>
+  <ue-button-group size="sm">
+    <ue-button type="primary">Default Button</ue-button>
+    <ue-button type="success">Default Button</ue-button>
+    <ue-button type="warning">Default Button</ue-button>
+    <ue-button type="error">Default Button</ue-button>
+    <ue-button type="primary">Default Button</ue-button>
+  </ue-button-group>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
