16fc85674211246e30d4b6a85a84dff032cdcdc0
EmmaLu-ux <genhualu22@gmail.com>
2025-08-04 19:14:11 +0800

    fix: fix checked checkbox not showing 3s wait time

--- packages/utils/types.js
+++ packages/utils/types.js
@@ -6,7 +6,11 @@ export function types() {
     const types = ['Number', 'String', 'Boolean', 'Array', 'Function', 'Null', 'Undefined', 'Set', 'Map', 'Object', 'Promise']
     types.forEach((item) => {
         const fun = `is${item}`
-        type[fun] = (value) => Object.prototype.toString.call(value) === `[object ${item}]`
+        type[fun] = (value) => {
+            const typeStr = Object.prototype.toString.call(value);
+            // 可识别普通函数和异步函数
+            return item === 'Function' ? typeStr === '[object Function]' || typeStr === '[object AsyncFunction]' : typeStr === `[object ${item}]`;
+        }
     })
     return type
 }
