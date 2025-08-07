efc993ef083bcc34eb8a6e4a33b4104d1a041278
EmmaLu-ux <genhualu22@gmail.com>
2025-08-07 14:28:36 +0800

    feat(switch): add switch styles, themes, and switch label

--- examples/src/components/SwitchExamp.vue
+++ examples/src/components/SwitchExamp.vue
@@ -1,5 +1,19 @@
 <template>
-  <ue-switch></ue-switch>
+  <ue-switch size="sm"></ue-switch>
+  <ue-switch size="sm" type="primary"></ue-switch>
+  <ue-switch size="sm" type="success"></ue-switch>
+  <ue-switch size="sm" type="warning"></ue-switch>
+  <ue-switch size="sm" type="error"></ue-switch>
+  <hr />
+  <ue-switch size="sm"></ue-switch>
+  <ue-switch size="md"></ue-switch>
+  <ue-switch size="lg"></ue-switch>
+  <ue-switch size="xl"></ue-switch>
+  <hr />
+  <ue-switch size="sm" onLabel="Open" offLabel="Close"></ue-switch>
+  <ue-switch size="md" onLabel="开启" offLabel="关闭"></ue-switch>
+  <ue-switch size="lg"></ue-switch>
+  <ue-switch size="xl"></ue-switch>
 </template>
 
 <script setup></script>
--- packages/components/switch/src/index.vue
+++ packages/components/switch/src/index.vue
@@ -1,10 +1,78 @@
 <template>
-  <div :class="[ns.b()]"></div>
+  <div
+    :class="[
+      ns.b(),
+      ns.m(type),
+      ns.m('size', size),
+      ns.is('disabled', disabled),
+      ns.is('checked', isChecked),
+    ]">
+    <div :class="[ns.e('input')]">
+      <input type="checkbox" :disabled="disabled" />
+      <span :class="[ns.e('handle')]">
+        <button
+          type="button"
+          :class="[
+            ns.e('button'),
+            ns.is('checked', isChecked),
+            ns.is('disabled', disabled),
+          ]"></button>
+        <span :class="[ns.e('inner')]">
+          <span :class="[ns.e('inner-checked')]">
+            <span v-if="onLabel"> {{ firstChatAt(onLabel) }}</span>
+          </span>
+          <span :class="[ns.e('inner-unchecked')]">
+            <span v-if="offLabel"> {{ firstChatAt(offLabel) }}</span>
+          </span>
+        </span>
+      </span>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { computed } from "vue"
 import { useNamespace } from "@ui-element-vue3/hooks"
 
 defineOptions({ name: "ue-switch" })
 const ns = useNamespace("switch")
+
+const props = defineProps({
+  size: {
+    type: String,
+    default: "sm",
+  },
+  disabled: Boolean,
+  type: {
+    type: String,
+    default: "primary",
+  },
+  onLabel: {
+    type: String,
+    default: "",
+  },
+  offLabel: {
+    type: String,
+    default: "",
+  },
+  //   checked: Boolean,
+})
+const isChecked = computed(() => true)
+// const isDisabled = computed(() => props.disabled)
+
+const firstChatAt = str => {
+  const value = str.trim()
+  if (!value) return ""
+
+  // 判断是否包含中文字符
+  const hasChinese = /[\u4e00-\u9fa5]/.test(value)
+
+  if (hasChinese) {
+    // 如果包含中文，只返回第一个字符
+    return value.charAt(0)
+  } else {
+    // 如果是英文，最多返回3个字符
+    return value.slice(0, 3)
+  }
+}
 </script>
